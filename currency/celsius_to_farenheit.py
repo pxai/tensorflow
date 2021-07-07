3036d306b63a4b4ac704fc298f2553c29e33eef2
pxai <pello.altadill@gluckgames.com>
2021-07-07 11:19:43 +0200

    Added currency sample

---
+++ currency/celsius_to_farenheit.py
@@ -0,0 +1,26 @@
+import tensorflow
+import numpy
+import matplotlib.pyplot as plot
+
+celsius = numpy.array([-40, -10, 0, 8, 15, 22, 38], dtype = float)
+farenheit = numpy.array([-40, 14, 32, 46, 59, 72, 100], dtype = float)
+
+layer = tensorflow.keras.layers.Dense(units = 1, input_shape = [1])
+model = tensorflow.keras.Sequential([layer])
+LEARNING_RATE = 0.1
+model.compile(
+    optimizer = tensorflow.keras.optimizers.Adam(LEARNING_RATE),
+    loss = 'mean_squared_error'
+)
+
+print("Start learning...")
+history = model.fit(celsius, farenheit, epochs = 1000, verbose = False)
+print("Learning completed!!")
+
+result = model.predict([100.0])
+print("Prediction for 100.0 is %s" % result)
+
+plot.xlabel("#Iteration")
+plot.ylabel("#Loss")
+plot.plot(history.history["loss"])
+plot.show()
